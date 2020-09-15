91a8bb4ea35937103cddd73524ae8d31b28beb27
Pedro Santos <pedrocarlos.ti@gmail.com>
2020-09-15 17:23:59 -0300

    add: navigation package and route

---
+++ App/index.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import List from "./screens/List";
+import Details from "./screens/Details";
+
+import { AddButton } from "./components/Navigation";
+
+const Stack = createStackNavigator();
+
+const AppWithContainer = () => (
+  <NavigationContainer>
+    <Stack.Navigator
+      screenOptions={{
+        headerTitleAlign: "center",
+        headerStyle: { backgroundColor: "#3A8552" },
+        headerTintColor: "#fff",
+      }}
+    >
+      <Stack.Screen
+        name="Caches"
+        component={List}
+        options={({ navigation }) => ({
+          headerRight: () => <AddButton navigation={navigation} />,
+        })}
+      />
+      <Stack.Screen
+        name="Details"
+        component={Details}
+        options={({ route }) => ({
+          title: route.params?.title,
+        })}
+      />
+    </Stack.Navigator>
+  </NavigationContainer>
+);
+
+const App = () => (
+  <React.Fragment>
+    <StatusBar style="light" />
+    <AppWithContainer />
+  </React.Fragment>
+);
+
+export default App;
